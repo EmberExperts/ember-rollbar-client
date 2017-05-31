360151879049529ea1ec431df340b616fbe3b2ae
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-05-31 18:13:03 +0200

    Add tests

--- tests/unit/services/rollbar-test.js
+++ tests/unit/services/rollbar-test.js
@@ -1,12 +1,46 @@
 import { moduleFor, test } from 'ember-qunit';
+import Rollbar from 'rollbar';
 
 moduleFor('service:rollbar', 'Unit | Service | rollbar', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  needs: ['config:environment']
 });
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
   let service = this.subject();
   assert.ok(service);
+});
+
+test('notifier', function(assert) {
+  let service = this.subject();
+  assert.ok(service.get('notifier') instanceof Rollbar);
+});
+
+test('critical', function(assert) {
+  let service = this.subject();
+  let uuid = service.critical('My error message').uuid;
+  assert.ok(uuid);
+});
+
+test('error', function(assert) {
+  let service = this.subject();
+  let uuid = service.error('My error message').uuid;
+  assert.ok(uuid);
+});
+
+test('warning', function(assert) {
+  let service = this.subject();
+  let uuid = service.warning('My error message').uuid;
+  assert.ok(uuid);
+});
+
+test('info', function(assert) {
+  let service = this.subject();
+  let uuid = service.info('My error message').uuid;
+  assert.ok(uuid);
+});
+
+test('debug', function(assert) {
+  let service = this.subject();
+  let uuid = service.debug('My error message').uuid;
+  assert.ok(uuid);
 });
