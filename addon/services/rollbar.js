06cde729c8f8bacb2b5eaa13a57bd241401a1533
Dawid Pośliński <dposlinski@gmail.com>
2017-11-11 13:46:28 +0100

    Deep merge

--- addon/services/rollbar.js
+++ addon/services/rollbar.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import Rollbar from 'rollbar';
+import deepMerge from 'lodash/merge';
 
 export default Ember.Service.extend({
   currentUser: null,
@@ -20,7 +21,7 @@ export default Ember.Service.extend({
   }),
 
   rollbarClient(customConfig = {}) {
-    let config = Ember.assign(this.get('config'), customConfig);
+    let config = deepMerge(this.get('config'), customConfig);
     return new Rollbar(config);
   },
 
