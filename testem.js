ab675172a304e11e3f6829ea621c5986767c7a8d
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-11-28 09:55:46 +0100

    Updates

--- testem.js
+++ testem.js
@@ -1,11 +1,11 @@
 /* eslint-env node */
 module.exports = {
-  'test_page': 'tests/index.html?hidepassed',
-  'disable_watching': true,
-  'launch_in_ci': [
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: [
     'Chrome'
   ],
-  'launch_in_dev': [
+  launch_in_dev: [
     'Chrome'
   ],
   browser_args: {
