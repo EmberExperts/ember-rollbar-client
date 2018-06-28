656c94f54ed96397f833255087c706008e2fe1d2
Scott Vitale <scott@spigotlabs.com>
2018-06-27 18:47:26 -0600

    Revert "Remove funnel"
    This reverts commit b9db51a8bfb4765612db63000b72525a7657199f.
    
    # Conflicts:
    #	index.js
    #	package-lock.json

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 'use strict';
 
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+
 module.exports = {
   name: 'ember-rollbar-client',
 
   included: function(app) {
     this._super.included(app);
-    app.import('node_modules/rollbar/dist/rollbar.named-amd.js')
+    app.import('vendor/ember-rollbar-client/rollbar.named-amd.js');
+  },
+
+  rollbarPath: function() {
+    return path.join(this.app.project.root, 'node_modules', 'rollbar', 'dist');
+  },
+
+  treeForVendor: function() {
+    return new Funnel(this.rollbarPath(), {
+      destDir: 'ember-rollbar-client',
+      files: ['rollbar.named-amd.js']
+    });
   }
 };
