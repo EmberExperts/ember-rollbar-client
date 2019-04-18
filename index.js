259bfaf9b7c34f0231d0eb09bc29c61d2d170ae0
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-04-18 17:54:43 +0200

    Migrate to ember-auto-import

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
 'use strict';
 
-const path = require('path');
-const Funnel = require('broccoli-funnel');
-
 module.exports = {
-  name: require('./package').name,
-
-  importTransforms: require('ember-cli-cjs-transform').importTransforms,
-
-  included: function(app) {
-    this._super.included(app);
-    app.import('vendor/ember-rollbar-client/rollbar.named-amd.js');
-    app.import('node_modules/lodash.merge/index.js', {
-      using: [
-        { transformation: 'cjs', as: 'lodash.merge' }
-      ]
-    });
-  },
-
-  rollbarPath: function() {
-    var rollbarPath = path.dirname(this.project.resolveSync('rollbar/package.json'));
-    return path.join(rollbarPath, 'dist');
-  },
-
-  treeForVendor: function() {
-    let files = ['rollbar.named-amd.js'];
-
-    if (this.app.options.sourcemaps.enabled) {
-      files.push('rollbar.named-amd.js.map');
-    }
-
-    return new Funnel(this.rollbarPath(), {
-      destDir: 'ember-rollbar-client',
-      files,
-    });
-  }
+  name: require('./package').name
 };
